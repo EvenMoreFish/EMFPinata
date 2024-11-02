7bd681951750eca9098bb8dd186bad5ba2c10de4
FireML <fireml@firedev.uk>
2024-11-02 17:01:02 +0000

    Update Shadow to 8.3.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ dependencyResolutionManagement {
             library("bstats", "org.bstats:bstats-bukkit:3.0.2")
             library("mythicmobs", "io.lumine:Mythic-Dist:5.7.1")
 
-            plugin("shadow", "com.gradleup.shadow").version("8.3.1")
+            plugin("shadow", "com.gradleup.shadow").version("8.3.3")
             plugin("plugin-yml", "net.minecrell.plugin-yml.bukkit").version("0.6.0")
         }
     }
