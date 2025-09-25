1d9b0d1ef8ac6ffee2647aac67f8abdad5639de4
FireML <fireml@firedev.uk>
2025-09-25 16:39:16 +0100

    Fix message prefix

--- src/main/java/uk/firedev/emfpinata/config/MessageConfig.java
+++ src/main/java/uk/firedev/emfpinata/config/MessageConfig.java
@@ -4,6 +4,7 @@ import org.jetbrains.annotations.NotNull;
 import uk.firedev.emfpinata.EMFPinata;
 import uk.firedev.messagelib.message.ComponentMessage;
 import uk.firedev.messagelib.message.ComponentSingleMessage;
+import uk.firedev.messagelib.replacer.Replacer;
 
 import static uk.firedev.messagelib.message.ComponentMessage.componentMessage;
 
@@ -25,30 +26,30 @@ public class MessageConfig extends ConfigBase {
         return instance;
     }
 
-    private @NotNull ComponentMessage applyPrefix(@NotNull ComponentMessage component) {
-        return component.replace("{prefix}", getPrefix());
-    }
-
     // GENERAL
 
     public ComponentSingleMessage getPrefix() {
         return componentMessage(loader, "prefix", "<gray>[EMFPinata] </gray>").toSingleMessage();
     }
 
+    public Replacer getPrefixReplacer() {
+        return Replacer.replacer().addReplacement("{prefix}", getPrefix().get());
+    }
+
     // MAIN COMMAND
 
     public ComponentMessage getReloadedMessage() {
-        return componentMessage(loader, "main-command.reloaded", "{prefix}<aqua>Successfully reloaded the plugin.");
+        return componentMessage(loader, "main-command.reloaded", "{prefix}<aqua>Successfully reloaded the plugin.").replace(getPrefixReplacer());
     }
 
     // PINATA COMMAND
 
     public ComponentMessage getPinataNotValidMessage() {
-        return componentMessage(loader, "pinata-command.not-valid", "{prefix}<red>That piñata does not exist!");
+        return componentMessage(loader, "pinata-command.not-valid", "{prefix}<red>That piñata does not exist!").replace(getPrefixReplacer());
     }
 
     public ComponentMessage getPinataSpawnedMessage() {
-        return componentMessage(loader, "pinata-command.spawned", "{prefix}<aqua>Successfully spawned a Piñata.");
+        return componentMessage(loader, "pinata-command.spawned", "{prefix}<aqua>Successfully spawned a Piñata.").replace(getPrefixReplacer());
     }
 
 }
