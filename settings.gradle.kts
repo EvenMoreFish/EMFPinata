d06efe1b936ccce9c42c935ad97b688448bbc78a
FireML <fireml@firedev.uk>
2025-11-11 09:00:20 +0000

    Set version to 2.0.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ dependencyResolutionManagement {
             library("bstats", "org.bstats:bstats-bukkit:3.1.0")
             library("mythicmobs", "io.lumine:Mythic-Dist:5.6.2")
 
-            library("evenmorefish", "com.oheers.evenmorefish:even-more-fish-api:2.1.0-SNAPSHOT")
+            library("evenmorefish", "com.oheers.evenmorefish:even-more-fish-api:2.1.1")
 
             library("messagelib", "uk.firedev:MessageLib:1.0.3")
 
